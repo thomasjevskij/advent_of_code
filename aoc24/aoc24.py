8cb3eea95bab0dc2ccd4ad167a88f5dc6a024cb5
Thomas Sievert <thomas.sievert@bth.se>
2015-12-24 08:55:58 +0100

    Tidied Day 24
    Didn't need to store combinations, use reduce right away

--- aoc24/aoc24.py
+++ aoc24/aoc24.py
@@ -11,21 +11,16 @@ p1_loads = []
 p2_loads = []
 done = False
 i = 1
+p1 = p2 = 2**63
 while not done:
     for c in combinations(packages, i):
         if sum(c) * 4 == all_loads:
-            p2_loads.append(c)
+            p2 = min(p2, reduce(lambda x, y: x * y, c))
         if sum(c) * 3 == all_loads:
-            p1_loads.append(c)
+            p1 = min(p1, reduce(lambda x, y: x * y, c))
             done = True        
     i += 1
-p1 = 2**63
-for l in p1_loads:
-    p1 = min(p1, reduce(lambda x, y: x * y, l))
-print("Problem 1: %d"%p1)
-p2 = 2**63
-for l in p2_loads:
-    p2 = min(p2, reduce(lambda x, y: x * y, l))
 t = pt() - t
+print("Problem 1: %d"%p1)
 print("Problem 2: %d"%p2)
 print("Time elapsed: %d ms"%int(t * 1000))
