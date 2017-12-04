388d69001acd6e61d4cfa67b273020b770c99ec0
Thomas Sievert <thomas.sievert@bth.se>
2017-12-04 13:00:17 +0100

    Golfed 4 some more
    It's even better now.

--- 2017/aoc04/04.py
+++ 2017/aoc04/04.py
@@ -1,2 +1,1 @@
-print(sum(len(l.split())==len(set(l.split())) for l in open('in')))
-print(sum(len(l.split())==len(set(tuple(sorted(w)) for w in l.split())) for l in open('in')))
+print(sum(len(l.split())==len(set(l.split())) for l in open('in')), sum(len(l.split())==len(set(tuple(sorted(w)) for w in l.split())) for l in open('in')), sep='\n')
