08aea713f9bf9789b25d204d3bda36f4d43e6565
Thomas Sievert <thomas.sievert@bth.se>
2017-12-06 10:53:12 +0100

    Day 6 polish
    I was dumb before. No need to count.

--- 2017/aoc06/06.py
+++ 2017/aoc06/06.py
@@ -1,11 +1,5 @@
 import time
 
-def forever(start = 0):
-    i = start
-    while True:
-        yield i
-        i += 1
-
 with open('in') as f:
     banks = list(map(int, f.readline().split()))
 
@@ -14,7 +8,7 @@ t = time.process_time()
 c2 = 0
 visited = list()
 visited.append(tuple(banks))
-for c in forever(1):
+while True:
     i, v = max(enumerate(banks), key=lambda x: x[1])
     banks[i] = 0
     while v > 0:
@@ -24,11 +18,11 @@ for c in forever(1):
         v -= 1
     state = tuple(banks)
     if state in visited:
-        c2 = c-visited.index(state)
+        c2 = len(visited)-visited.index(state)
         break
     visited.append(state)
 
-print("Problem 1: {0}".format(c))
+print("Problem 1: {0}".format(len(visited)))
 print("Problem 2: {0}".format(c2))
 t = time.process_time() - t
 print("Time elapsed: {0:.2f} s".format(t))
