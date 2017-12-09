9e9f539284668dc68305f8f07ec4727db6caf2ac
Thomas Sievert <thomas.sievert@bth.se>
2017-12-09 21:48:21 +0100

    Changed day 9
    Now does not hold entire input in memory

--- 2017/aoc09/09.py
+++ 2017/aoc09/09.py
@@ -2,33 +2,32 @@ import time
 
 t = time.process_time()
 
-with open('in') as f:
-    stream = f.read()
-
 i = 0
 depth = 0
 p1 = 0
 p2 = 0
 ignore = False
-while i < len(stream):
-    c = stream[i]
-    if c == '!':
-        i += 2
-        continue
-    if not ignore:
-        if c == '{':
-            depth += 1
-        if c == '}':
-            p1 += depth
-            depth -= 1
-        if c == '<':
-            ignore = True
-    else:
-        if c == '>':
-            ignore = False
+with open('in') as f:
+    c = f.read(1)
+    while c != '':
+        if c == '!':
+            f.read(1)
+            c = f.read(1)
+            continue
+        if not ignore:
+            if c == '{':
+                depth += 1
+            if c == '}':
+                p1 += depth
+                depth -= 1
+            if c == '<':
+                ignore = True
         else:
-            p2 += 1
-    i += 1
+            if c == '>':
+                ignore = False
+            else:
+                p2 += 1
+        c = f.read(1)
 
 print("Problem 1: {}".format(p1))
 print("Problem 2: {}".format(p2))
