6efff1ee9e97165c4dc7ade44f85a16e4c13d246
Thomas Sievert <thomas.sievert@bth.se>
2017-12-14 13:23:03 +0100

    Day 14
    Changed names of files to be importable
    Still wasn't good, interpreted whole file
    Copied the entire function from 10
    Brute force solution.

--- 2017/aoc10/aoc10.py
+++ 2017/aoc10/aoc10.py
@@ -3,6 +3,23 @@ from collections import deque
 from functools import reduce
 from operator import xor
 
+def knot_hash(string):
+    lengths = list(map(ord, string))
+    lengths.extend([17, 31, 73, 47, 23])
+    curpos = 0
+    skip_size = 0
+    d = deque(range(256))
+
+    for _ in range(64):
+        for l in lengths:
+            temp = list(d)
+            d = deque(list(reversed(temp[:l]))+temp[l:])
+            curpos += l + skip_size
+            d.rotate(-l-skip_size)
+            skip_size += 1
+    d.rotate(curpos)
+    return ''.join('{0:02x}'.format(reduce(xor, list(d)[i:i+16])) for i in range(0, len(d), 16))
+
 t = time.process_time()
 
 with open('in') as f:
@@ -28,24 +45,6 @@ print("Time elapsed: {0:.2f} s".format(t))
 
 t = time.process_time()
 
-lengths_2 = list(map(ord, in_string))
-lengths_2.extend([17, 31, 73, 47, 23])
-curpos = 0
-skip_size = 0
-d = deque(range(256))
-
-for _ in range(64):
-    for l in lengths_2:
-        temp = list(d)
-        d = deque(list(reversed(temp[:l]))+temp[l:])
-        curpos += l + skip_size
-        d.rotate(-l-skip_size)
-        skip_size += 1
-d.rotate(curpos%len(d))
-
-sparse_hash = list(d)
-dense_hash = ''.join('{0:02x}'.format(reduce(xor, sparse_hash[i:i+16])) for i in range(0, len(d), 16))
-
-print("Problem 2: {}".format(dense_hash))
+print("Problem 2: {}".format(knot_hash(in_string)))
 t = time.process_time() - t
 print("Time elapsed: {0:.2f} s".format(t))
