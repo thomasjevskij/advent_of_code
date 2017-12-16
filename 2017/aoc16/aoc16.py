ad939a17c24b424c401d7510d04a6efb98086e50
Thomas Sievert <thomas.sievert@bth.se>
2017-12-16 15:08:53 +0100

    Day 16 polish
    Made a function

--- 2017/aoc16/aoc16.py
+++ 2017/aoc16/aoc16.py
@@ -11,16 +11,19 @@ def exchange(s, args):
 def partner(s, args):
     x, y = args.split('/')
     return s.replace(x, '_').replace(y, x).replace('_', y)
+def dance(dance_moves, programs, moves):
+    for move in dance_moves.split(','):
+        s, args = move[0], move[1:]
+        programs = moves[s](programs, args)
+    return programs
 
 moves = {'s' : spin, 'x' : exchange, 'p' : partner}
 programs = 'abcdefghijklmnop'
 
 with open('in') as f:
-    dance = f.read().strip()
+    dance_moves = f.read().strip()
 
-for move in dance.split(','):
-    s, args = move[0], move[1:]
-    programs = moves[s](programs, args)
+programs = dance(dance_moves, programs, moves)
 
 t = time.process_time() - t 
 print(f'Problem 1: {programs}')
@@ -29,14 +32,12 @@ print(f"Time elapsed: {t:.2f} s")
 t = time.process_time()
 visited = [programs]
 while True:
-    for move in dance.split(','):
-        s, args = move[0], move[1:]
-        programs = moves[s](programs, args)
+    programs = dance(dance_moves, programs, moves)
     if programs not in visited:
         visited.append(programs)
     else:
         break
 
 t = time.process_time() - t
-print(f'Problem 2: {visited[999999999 % len(visited)]}')
+print(f'Problem 2: {visited[int("9"*9) % len(visited)]}')
 print(f"Time elapsed: {t:.2f} s")
