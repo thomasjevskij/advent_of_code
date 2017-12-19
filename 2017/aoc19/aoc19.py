34b7b66fca6011bfc45e3c9cde09217660d1ab6f
Thomas Sievert <thomas.sievert@bth.se>
2017-12-19 20:12:16 +0100

    Polish
    Added lambda functions just cuz

--- 2017/aoc19/aoc19.py
+++ 2017/aoc19/aoc19.py
@@ -6,37 +6,34 @@ with open('in') as f:
     diagram = f.read().split('\n')
 
 direction = 1 + 0j
-row = 0
-col = diagram[row].find('|')
-pos = row + col*1j
+pos = 0
+pos += 1j*diagram[0].find('|')
 log = ''
 counter = 1
-
+getRowCol = lambda z: map(int, (z.real, z.imag))
+outOfBounds = lambda row, col, diagram: row < 0 or col < 0 or row >= len(diagram) or col >= len(diagram[row])
 while True:
     counter += 1
     pos += direction
-    row, col = map(int, (pos.real, pos.imag))
+    row, col = getRowCol(pos)
     c = diagram[row][col]
     if c.isalpha():
         log += c
     if c == '+':
-        newdirs = [direction * 1j, direction * -1j]
-        for nd in newdirs: # Look ahead
-            test_pos = pos + nd
-            test_row, test_col = map(int, (test_pos.real, test_pos.imag))
-            if test_row < 0 or test_col < 0 or test_row >= len(diagram) or test_col >= len(diagram[row]):
+        for nd in (direction * 1j, direction * -1j): # Look ahead, multiplication with 1j is rotation
+            row, col = getRowCol(pos + nd)
+            if outOfBounds(row, col, diagram):
                 continue # If this direction takes us out of bounds we don't wanna check the array
-            if diagram[test_row][test_col] != ' ':
+            if diagram[row][col] != ' ':
                 direction = nd
                 break
         continue
-    test_row, test_col = map(int, ((pos + direction).real, (pos + direction).imag))
-    if test_row < 0 or test_col < 0 or test_row >= len(diagram) or test_col >= len(diagram[row]):
+    row, col = getRowCol(pos + direction)
+    if outOfBounds(row, col, diagram):
         break # If this direction takes us out of bounds we don't wanna check the array
-    if diagram[test_row][test_col] == ' ': # If there's no plus but next step is a whitespace we're done
+    if diagram[row][col] == ' ': # If there's no plus but next step is a whitespace we're done
         break
     
-
 t = time.process_time() - t
 print(f"Problem 1: {log}")
 print(f"Problem 2: {counter}")
