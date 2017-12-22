7e0a9fdffc70001595cef648c3b25fce6bbade3a
Thomas Sievert <thomas.sievert@bth.se>
2017-12-22 11:04:35 +0100

    More polish
    Removed unnecessary function
    Do it only once, then copy as you pass
    Better way

--- 2017/aoc22/aoc22.py
+++ 2017/aoc22/aoc22.py
@@ -1,13 +1,7 @@
 import time
 from collections import defaultdict
+from copy import copy
 
-def makeGrid(sGrid):
-    grid = defaultdict(lambda: '.')
-    mid = len(sGrid) // 2
-    for row in range(len(sGrid)):
-        for col in range(len(sGrid)):
-            grid[(col-mid, -(row-mid))] = sGrid[row][col]
-    return grid
 def burst(grid, rules, iterations): 
     pos = 0 + 0*1j
     direction = 0 + 1j
@@ -25,18 +19,21 @@ t = time.process_time()
 with open('in') as f:
     sGrid = f.read().split('\n')
 
-grid = makeGrid(sGrid)
+grid = defaultdict(lambda: '.')
+mid = len(sGrid) // 2
+for row in range(len(sGrid)):
+    for col in range(len(sGrid)):
+        grid[(col-mid, -(row-mid))] = sGrid[row][col]
 rules = {'#': ('.', -1j, 0), '.': ('#', 1j, 1)}
 
-print(f"Problem 1: {burst(grid, rules, 10**4)}")
+print(f"Problem 1: {burst(copy(grid), rules, 10**4)}")
 t = time.process_time() - t
 print(f"Time elapsed: {t:.2f} s")
 t = time.process_time()
 
-grid = makeGrid(sGrid)
 rules = {'#': ('F', -1j, 0), '.': ('W', 1j, 0),
          'W': ('#', 1, 1), 'F': ('.', -1, 0)}
     
-print(f"Problem 2: {burst(grid, rules, 10**7)}")
+print(f"Problem 2: {burst(copy(grid), rules, 10**7)}")
 t = time.process_time() - t
 print(f"Time elapsed: {t:.2f} s")
