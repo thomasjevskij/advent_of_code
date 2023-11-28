09b83dad816c27877e9ebcb7fd64094f20565559
Thomas <thomas.t.m.sievert@gmail.com>
2023-11-28 14:01:20 +0100

    Day 01 solved!
    First day sold, happy christmas :)

--- 2023/aoc01/day01.py
+++ 2023/aoc01/day01.py
@@ -1,1 +1,22 @@
-print("Hello, AOC 2023!")
+import re
+
+with open(0) as f:
+    lines = f.readlines()
+
+# part 1
+s = 0
+t = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+     'one': 1, 'two': 2, 'three':3, 'four':4, 'five':5, 'six':6,
+     'seven':7, 'eight':8, 'nine':9}
+for l in lines:
+    nums = re.findall(r'\d', l.strip())
+    s += t[nums[0]]*10 + t[nums[-1]]
+print(s)
+
+# part 2
+s = 0
+for l in lines:
+    pattern = r'\d|one|two|three|four|five|six|seven|eight|nine'
+    nums = re.findall(pattern, l.strip())
+    s += t[nums[0]]*10 + t[nums[-1]]
+print(s)
