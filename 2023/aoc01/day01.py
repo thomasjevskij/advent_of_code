2328b566c4e280e02a73758743fadf20e819783c
Thomas <thomas.t.m.sievert@gmail.com>
2023-12-03 17:49:29 +0100

    Fixed day 01
    Before it couldn't handle "twone" properly. I was lucky.
    Now it is fixed.

--- 2023/aoc01/day01.py
+++ 2023/aoc01/day01.py
@@ -2,25 +2,27 @@ import re
 
 def parse_input():
     with open(0) as f:
-        lines = f.readlines()
+        lines = [l.strip() for l in f.readlines()]
     return lines
 
 def p1(puzzle_input):
-    s = 0
-    for l in puzzle_input:
-        nums = re.findall(r'\d', l.strip())
-        s += int(nums[0])*10 + int(nums[-1])
+    def make_number(line):
+        nums = re.findall(r'\d', line)
+        return int(nums[0] + nums[-1])
+    
+    s = sum(make_number(l) for l in puzzle_input)
     print(s)
 
 def p2(puzzle_input):
-    s = 0
-    t = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
-        'one': 1, 'two': 2, 'three':3, 'four':4, 'five':5, 'six':6,
-        'seven':7, 'eight':8, 'nine':9}
-    for l in puzzle_input:
-        pattern = r'\d|one|two|three|four|five|six|seven|eight|nine'
-        nums = re.findall(pattern, l.strip())
-        s += t[nums[0]]*10 + t[nums[-1]]
+    def make_number(line):
+        t = {'1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+            'one': 1, 'two': 2, 'three':3, 'four':4, 'five':5, 'six':6,
+            'seven':7, 'eight':8, 'nine':9}
+        pattern1 = r'(\d|one|two|three|four|five|six|seven|eight|nine)'
+        pattern2 = r'(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)'
+        return 10 * t[re.search(pattern1, line)[0]] + t[re.search(pattern2, line[::-1])[0][::-1]]
+    
+    s = sum(make_number(l) for l in puzzle_input)
     print(s)
 
 puzzle_input = parse_input()
