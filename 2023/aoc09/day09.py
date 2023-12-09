54a4871d59b2f1f1b03a99fd7e69569e298e22c1
Thomas <thomas.t.m.sievert@gmail.com>
2023-12-09 15:33:34 +0100

    Cleanup day 09
    Now it is a lot prettier

--- 2023/aoc09/day09.py
+++ 2023/aoc09/day09.py
@@ -1,35 +1,26 @@
 from itertools import pairwise
+from functools import reduce
+from operator import sub
 
 def parse_input():
     with open(0) as f:
         lines = [l.strip() for l in f.readlines()]
-    sequences = [[int(n) for n in l.split()] for l in lines]
+    sequences = tuple([int(n) for n in l.split()] for l in lines)
     return sequences
 
+def diff(seq):
+    seqs = [seq]
+    while True:
+        seqs.insert(0, tuple(s2 - s1 for s1, s2 in pairwise(seqs[0])))
+        if len(set(seqs[0])) == 1:
+            return seqs
+
 def p1(sequences):
-    def process_seq(seq):
-        seqs = [seq]
-        while True:
-            seqs.insert(0, [s2 - s1 for s1, s2 in pairwise(seqs[0])])
-            if len(set(seqs[0])) == 1:
-                break
-        for i in range(len(seqs)-1):
-            seqs[i+1].append(seqs[i][-1]+seqs[i+1][-1])
-        return seqs[-1][-1]
-    s = sum(process_seq(s[:]) for s in sequences)
+    s = sum(sum(ss[-1] for ss in diff(seq)) for seq in sequences)
     print(s)
 
 def p2(sequences):
-    def process_seq(seq):
-        seqs = [seq]
-        while True:
-            seqs.insert(0, [s2 - s1 for s1, s2 in pairwise(seqs[0])])
-            if len(set(seqs[0])) == 1:
-                break
-        for i in range(len(seqs)-1):
-            seqs[i+1].insert(0, seqs[i+1][0]-seqs[i][0])
-        return seqs[-1][0]
-    s = sum(process_seq(s[:]) for s in sequences)
+    s = sum(sum(ss[-1] for ss in diff(seq[::-1])) for seq in sequences)
     print(s)
 
 sequences = parse_input()
