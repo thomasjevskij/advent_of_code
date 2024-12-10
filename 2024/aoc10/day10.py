9c05e9f2cd771d596b9e7bc05910153933408bd9
Thomas Sievert <thomassi@axis.com>
2024-12-10 09:57:05 +0100

    Finished day 10
    Very nice and clean graph traversal :)
    
    Change-Id: I98737b807895f7781730062f4f091459684e217b

---
+++ 2024/aoc10/day10.py
@@ -0,0 +1,56 @@
+from itertools import product
+from collections import deque
+
+def parse_input(filename=0):
+    with open(filename) as f:
+        grid = [[int(c) for c in l.strip()] for l in f.readlines()]
+    return grid
+
+def get_next(grid, x, y):
+    candidates = [
+        (xn, yn) for xn, yn in 
+        ((x-1, y), (x, y-1), (x+1, y), (x, y+1))
+        if 0 <= xn < len(grid[0]) and 0 <= yn < len(grid)
+        and grid[yn][xn] == grid[y][x] + 1
+    ]
+    return candidates
+
+def p1(grid):
+    trailheads = [(x, y) for x, y in
+        product(range(len(grid[0])), range(len(grid)))
+        if grid[y][x] == 0]
+    score = 0
+    for head in trailheads:
+        stack = []
+        stack.append(head)
+        visited = set()
+        while stack:
+            v = stack.pop(0)
+            if v not in visited:
+                visited.add(v)
+                stack.extend(get_next(grid, *v))
+        score += sum(grid[y][x] == 9 for x, y in visited)
+    print(score)
+
+def p2(grid):
+    trailheads = [(x, y) for x, y in product(
+        range(len(grid[0])), range(len(grid))
+        ) if grid[y][x] == 0]
+    rating = 0
+    for head in trailheads:
+        queue = deque()
+        queue.append([head])
+        while queue:
+            path = queue.popleft()
+            x, y = path[-1]
+            if grid[y][x] == 9:
+                rating += 1
+            for adjacent in get_next(grid, x, y):
+                new_path = list([*path, adjacent])
+                queue.append(new_path)
+    print(rating)
+
+puzzle_input = parse_input()
+
+p1(puzzle_input[:])
+p2(puzzle_input[:])
